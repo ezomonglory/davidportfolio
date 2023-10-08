9d91899ea51eab09c88aa745806bc305b04a3855
Ezomon Glory <ezomonglory01@gmail.com>
2023-10-08 15:45:12 +0100

    top-borders

--- components/LineTop.jsx
+++ components/LineTop.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const LineTop = () => {
   return (
     <div className='w-full  flex md:h-[34px] h-[36px] border-[1.5px]  border-transparent '>
-        <div className=' w-[16px] md:w-[80px] h-full border-[1.5px]  border-b-[#D7D7D7] border-r-[#D7D7D7]   border-transparent  ' >
+        <div className=' w-[16px] md:w-[80px] h-full border-[1.5px] !border-r-[0.75px]  border-b-[#D7D7D7] border-r-[#D7D7D7]   border-transparent  ' >
 
         </div>
 
-        <div className='w-full h-full border-b-[#D7D7D7]  border-[1.5px] border-transparent ' >
+        <div className='w-full h-full border-b-[#D7D7D7] border-l-[#D7D7D7] border-r-[#D7D7D7]   border-[0.75px] border-transparent ' >
     
         </div>
 
-        <div className=' w-[16px] md:w-[80px] h-full border-[1.5px] border-b-[#D7D7D7] border-l-[#D7D7D7]  border-transparent ' >
+        <div className=' w-[16px] md:w-[80px] h-full border-[1.5px] border-b-[#D7D7D7] border-l-[#D7D7D7]  border-transparent  !border-l-[0.75px] ' >
 
         </div>
     </div>
