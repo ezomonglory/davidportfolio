636c9f467030b4ba4d28598132e0714ae756fd8a
Ezomon Glory <ezomonglory01@gmail.com>
2023-10-09 14:30:00 +0100

    changes

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
 	return (
 		<div className='flex flex-col  justify-between  md:flex-row mx-[16px] md:mx-[70px] border-[1px] md:border-t-[#D7D7D7] border-transparent md:pt-[40px] md:mt-[56px] mt-[20px] pt-[32px]  '>
 			<div className='md:w-[50%] flex space-y-[24px]  flex-col w-full  '>
-				<h1 className='text-[#212121] neue-md text-[30px] leading-[36px]  md:text-[46px] md:leading-[50px] tracking-[-1px]  mb-[8px]  '>
+				<h1 className='text-[#212121] libre-reg text-[30px] leading-[36px]  md:text-[46px] md:leading-[50px] tracking-[-2px] md:tracking-[-3px]  mb-[8px]  '>
 					Hi there, <br />
 					i’m David Otu
 				</h1>
@@ -17,7 +17,7 @@ const About = () => {
 			</div>
 
 			<div className='md:w-[50%] space-y-[24px] '>
-				<p className='text-[#505050] neue-reg md:text-[16px] md:leading-[28px] tracking-[-1%] md:tracking-[-2%] text-[14px] leading-[24px] mb-[8px]   '>
+				<p className='text-[#505050] neue-reg md:text-[16px] md:leading-[28px] tracking-[-1%] md:tracking-[-0.5px] text-[14px] leading-[24px] mb-[8px]   '>
 					{/* I started out as a meme enthusiast creating funny visuals when i
 					discovered Corel draw during my secondary school days. Overtime, i
 					ascended the ranks and evolved into a professional graphic designer.
@@ -49,11 +49,11 @@ const About = () => {
 							... More
 						</span>
 					)} */}
-					My journey commenced as a meme enthusiast, where I <br className="md:hidden" /> honed my creativity
-					using Corel Draw during secondary <br className="md:hidden" /> school. Over time, I progressed into
-					a seasoned graphic  <br className="md:hidden" />  designer, delving into presentation design. Today,  <br className="md:hidden" /> 
-					I specialize in product design, continuously refining my <br className="md:hidden" />  skills.My
-					latest exploration is in motion design, a passion  <br className="md:hidden" /> I pursue alongside
+					My journey commenced as a meme enthusiast, where I <br className="" /> honed my creativity
+					using Corel Draw during secondary <br className="" /> school. Over time, I progressed into
+					a seasoned graphic  <br className="" />  designer, delving into presentation design. Today,  <br className="" /> 
+					I specialize in product design, continuously refining my <br className="" />  skills.My
+					latest exploration is in motion design, a passion  <br className="" /> I pursue alongside
 					my professional endeavors.
 				</p>
 				{/* <div className='border-[1px] border-[#e22] rounded-[24px] md:px-[20px] md:py-[10px] px-[24px] py-[10px] md:text-[18px] neue-md md:leading-[28px] md:tracking-[-0.54px] text-[#e22]  space-x-[8px] items-center inline-flex  md:hidden w-max '>
--- components/HeroTop.jsx
+++ components/HeroTop.jsx
@@ -32,11 +32,11 @@ const HeroTop = () => {
 	return (
 		<div className='flex justify-between p-[4px] '>
 			{pathname === "/" ? (
-				<h1 className='neue-md md:text-[18px] text-[16px] leading-[24px] tracking-[-0.5px] md:leading-[28px] md:tracking-[-0.36px]   '>
+				<h1 className='neue-md md:text-[18px] text-[16px] leading-[24px] tracking-[-0.5px] md:leading-[28px] md:tracking-[-0.5px]   '>
 					David Otu
 				</h1>
 			) : (
-				<h1 className='neue-md md:text-[18px] text-[16px] leading-[24px] tracking-[-0.5px] md:leading-[28px] md:tracking-[-0.36px] text-gradient cursor-pointer  '
+				<h1 className='neue-md md:text-[18px] text-[16px] leading-[24px] tracking-[-0.5px] md:leading-[28px] md:tracking-[-0.5px] text-gradient cursor-pointer  '
                 onClick={()=> {
                     router.push("/")
                 }}
@@ -45,12 +45,12 @@ const HeroTop = () => {
 				</h1>
 			)}
 
-			<div className='flex order-1 items-center space-x-[6px] md:space-x-[8px] '>
-				<p className='md:leading-[28px] md:tracking-[-0.18px] neue-md text-[16px] leading-[24px]  tracking-[-0.5px] md:text-[18px] text-[#000] order-1'>
+			<div className='flex order-1 items-center space-x-[8px] md:space-x-[8px] '>
+				<p className='md:leading-[28px] md:tracking-[-0.5px] neue-md text-[16px] leading-[24px]  tracking-[-0.5px] md:text-[18px] text-[#000] order-1'>
 					{date?.getHours()}:{Minutes()}{" "}
 				</p>
 
-				<p className='text-[#787878] text-[16px] tracking-[-0.5px] leading-[24px] neue-reg md:text-[18px] md:leading-[28px] md:tracking-[-0.18px] neue-md order-2 '>
+				<p className='text-[#787878] text-[16px] tracking-[-0.5px] leading-[24px] neue-reg md:text-[18px] md:leading-[28px] md:tracking-[-0.5px] neue-md order-2 '>
 					Lagos,NG
 				</p>
 			</div>
